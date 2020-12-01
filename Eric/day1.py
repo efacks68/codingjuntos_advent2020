e329c92085ddd341f21f73a98ad785c412369334
efacks68 <fackelman.eric@gmail.com>
2020-12-01 22:26:38 +0100

    starting on my day 1 code with help from Lupe
    can read in the input file.

---
+++ Eric/day1.py
@@ -0,0 +1,10 @@
+#Day 1 - find the two expenses that add to 2020 and then multiply them. Return the product.
+
+f = open('input_day1.txt', 'r')
+lines = f.readlines()
+#print(lines)
+
+for number in lines:
+    print(int(number))
+    
+
