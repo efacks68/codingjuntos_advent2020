e7ef5170e56cb7f511e9c682aff9160aee82ee73
guadabsb15 <guadabsb15@gmail.com>
2020-12-02 20:53:04 +0100

    first solution

--- Lupe/day1.jl
+++ Lupe/day1.jl
@@ -5,5 +5,24 @@ println("hello world")
 
 # reading the input 
 data = readdlm("input_day1",Int)
-println(data)
+
+#loop through the elements in the array
+for i in eachindex(data)
+  #select number in the array  
+   n1 = data[i]   
+   data2 = data[i:end]
+
+   #compare (sum) with the rest of the numbers in the array
+   for j in eachindex(data2)
+     n2 = data2[j]     
+     sum = n1 + n2
+     if sum == 2020
+       println(n1)
+       println(n2)
+       println(n1*n2)
+     end
+
+   end
+
+end
 
