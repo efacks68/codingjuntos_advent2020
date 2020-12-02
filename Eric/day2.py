17b0db9e9cce726f67e701cf705e685e4b0b55e0
efacks68 <fackelman.eric@gmail.com>
2020-12-02 21:39:37 +0100

    day1 with better iterators and 2 first pass

---
+++ Eric/day2.py
@@ -0,0 +1,14 @@
+#Day2: identify how many passwords are valid according to their policies
+
+f=open("input_day2.txt",'r')
+lines=f.read()
+print(lines)
+
+sum=0
+
+strs=[]
+nums=[]
+
+for line in lines:
+   for ch in line:
+      if
